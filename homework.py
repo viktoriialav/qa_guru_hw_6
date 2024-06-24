407386d341a4b19acb91bb8786a3cff168f2c873
Viktoriia Lavrova <viktoriia.lav2908@gmail.com>
2024-06-24 23:18:18 +0300

    Update func "test_dark_theme_by_time_and_user_choice" in homework.py

--- homework.py
+++ homework.py
@@ -23,13 +23,19 @@ def test_dark_theme_by_time_and_user_choice():
     dark_theme_enabled_by_user = False - Темная тема выключена
     dark_theme_enabled_by_user = None - Пользователь не сделал выбор (используется переключение по времени системы)
     """
-    current_time = time(hour=16)
-    dark_theme_enabled_by_user = True
+
     # TODO переключите темную тему в зависимости от времени суток,
     #  но учтите что темная тема может быть включена вручную
+    def is_dark_theme(current_time=time(hour=16), dark_theme_enabled_by_user=True):
+        if dark_theme_enabled_by_user is None:
+            if current_time.hour >= 22 or current_time.hour < 6:
+                return True
+            else:
+                return False
+        else:
+            return dark_theme_enabled_by_user
 
-    is_dark_theme = None
-    assert is_dark_theme is True
+    assert is_dark_theme() is True
 
 
 def test_find_suitable_user():
